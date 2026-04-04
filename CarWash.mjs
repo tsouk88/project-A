cc42d20fd9f42002550d99d96681db04398e40d2
Kwstas <tsouk88@gmail.com>
2026-04-04 14:22:00 +0300

    added filesave and  fixedprice() ,weatherdata null

--- CarWash.mjs
+++ CarWash.mjs
@@ -1,3 +1,4 @@
+import { writeFile, readFile } from 'fs/promises';
 import  {Weather}  from './Weather.mjs';
 
 class CarWash {
@@ -34,24 +35,37 @@ addWash (vehicle , washType ) {
     this.dailyMoney = this.prices.reduce((total, price) => total + price, 0);
 
     }
+
+
 async Show () {
     console.log(`Today we had ${this.cars} cars and ${this.motorcycles} motorcycles.`);
     console.log(`We gathered ${this.dailyMoney}€ today. `);
     console.log(`People used ${this.simplewash} Simple Washes and ${this.premiumwash} Premium Washes today. `)
-    let temperature = await Weather();
-    if (temperature !== null) {
-        if (temperature > 15) {
-            console.log(`Temperature right now is ${temperature} Celsius , it's kinda hot`);
+    let weatherdata = await Weather();
+    if (weatherdata === null) {
+        console.log("Could not retrieve weather information right now.")
+        return }
+    if (weatherdata.currenttemp !== null) {
+        if (weatherdata.currenttemp > 15) {
+            console.log(`Temperature right now is ${weatherdata.currenttemp} Celsius , it's kinda hot`);
         } else {
-            console.log(`Temperature right now is ${temperature} Celsius , it's kinda cold`);
+            console.log(`Temperature right now is ${weatherdata.currenttemp} Celsius , it's kinda cold`);
         }
+        if (weatherdata.wind > 30) {
+            console.log(`Watchout: Wind at (${weatherdata.wind} km/h)!`);
+        }
+        console.log(`Tomorrow will have max ${weatherdata.tomorrowmax} Celsius and min ${weatherdata.tomorrowmin} Celsius.`)
     } else {
-        console.log("Could not retrieve temperature information right now.");
+        console.log("Could not retrieve weather information right now.");
     }
+    await this.SaveData();
 }
 
 fixedPrice() {
-    if (this.wash.washType === 'Simple') {
+    if(this.wash.vehicle !== 'Car' && this.wash.vehicle !== 'Motorcycle'){
+        console.log(`${this.wash.vehicle} is Not a Valid Entry` )
+        return}
+    else if (this.wash.washType === 'Simple') {
         this.simplewash++;
         return this.prices.push(this.simpleprice);
     } else if (this.wash.washType === 'Premium') {
@@ -59,8 +73,25 @@ fixedPrice() {
         return this.prices.push(this.premiumprice);
     }
     else {
-        console.log('Not valid Entry')
-        return;     
+        console.log(` ${this.wash.washType} is Not a Valid Entry` )
+    }
+}
+
+async SaveData () {
+    const Data = {
+        money : this.dailyMoney,
+        cars : this.cars,
+        motorcycles : this.motorcycles,
+        washes24h : this.washes24h,
+        simplewash : this.simplewash,
+        premiumwash : this.premiumwash,
+        date : new Date().toLocaleDateString()
+    }
+    try {       
+        await writeFile('wash_stats.json ', JSON.stringify(Data, null , 2))
+        console.log ("Τα δεδομένα αποθηκεύτηκαν με επιτυχία!")
+    } catch(err) {
+        console.error("Error Saving File" , err)
     }
 }
 }
@@ -69,8 +100,14 @@ const myStation = new CarWash();
 myStation.addWash('Car', 'Premium'); 
 myStation.addWash('Motorcycle', 'Simple'); 
 myStation.addWash('Car', 'Premium'); 
+myStation.addWash('Motorcycle', 'Simple'); 
+myStation.addWash('Car', 'Simple'); 
+myStation.addWash('Motorcycle', 'Premium');
+myStation.addWash('Car', 'VIP');
+myStation.addWash('Car', 'Premium');
 
 myStation.Show().catch(err => console.error(err));
+//localStorage.setItem('Total Money' ,this.dailyMoney) in case this was run on a browser
 
 
     
--- Weather.mjs
+++ Weather.mjs
@@ -2,9 +2,11 @@ export async function Weather() {
   try {
     const response = await fetch("https://wttr.in/Chios?format=j1");
     const data = await response.json();
-    //const humidity = data.current_condition[0].humidity;
     const temp = data.current_condition[0].temp_C;
-    return parseFloat(temp); 
+    const forecastTomorrow = data.weather[1];
+    return {currenttemp : parseFloat(temp) , tomorrowmin : parseFloat(forecastTomorrow.mintempC) , tomorrowmax : parseFloat(forecastTomorrow.maxtempC),
+      wind : parseFloat(data.current_condition[0].windspeedKmph)
+    };
   } 
   catch (error) {
     console.error("Weather fetch error:", error);
